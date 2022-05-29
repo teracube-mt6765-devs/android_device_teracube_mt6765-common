aaec218fd18682486ecdb47c8574e995aeb7cfdc
Gagan Malvi <malvigagan@gmail.com>
2022-05-29 17:49:21 +0200

    emerald: packages: Build modified FM Radio.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- emerald.mk
+++ emerald.mk
@@ -197,6 +197,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# FM Radio (modified)
+PRODUCT_PACKAGES += \
+    FMRadio
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
