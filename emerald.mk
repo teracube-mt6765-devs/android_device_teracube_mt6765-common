b1140bf89a79f76ec20b0ff8d74968655b9c6864
SebaUbuntu <barezzisebastiano@gmail.com>
2022-06-04 08:40:32 +0200

    emerald: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?

--- emerald.mk
+++ emerald.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
