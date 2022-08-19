f9469b4e6094c777321906e2ada2be1c76f63c07
SahilSonar <sss.sonar2003@gmail.com>
2022-08-19 10:41:09 +0200

    mt6765-common: Remove audio.a2dp.default from PRODUCT_PACKAGES

--- common.mk
+++ common.mk
@@ -108,7 +108,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.1-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
