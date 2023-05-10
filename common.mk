55d23875647ec03288f99abde239f640a75c7a35
Gagan Malvi <malvigagan@gmail.com>
2023-05-10 08:20:54 +0200

    mt6765-common: Move to developer GSI keys.
    * VNDK 33 removes gsi_keys.mk
    * https://android.googlesource.com/platform/build/+/master/target/product/developer_gsi_keys.mk
    
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- common.mk
+++ common.mk
@@ -47,7 +47,7 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Inherit cust configs, if any.
 $(call inherit-product-if-exists, emerald-cust/targets.mk)
